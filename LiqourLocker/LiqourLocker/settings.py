471edfbd6c5a8c0ef4897afde9e06ec057f240de
MatthewSidoti <coding.sidoti@gmail.com>
2025-01-11 00:56:23 -0500

    ready to deploy again

--- LiqourLocker/LiqourLocker/settings.py
+++ LiqourLocker/LiqourLocker/settings.py
@@ -79,10 +79,9 @@ WSGI_APPLICATION = 'LiqourLocker.wsgi.application'
 
 # Database
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default=f'sqlite:///{BASE_DIR / "db.sqlite3"}'
+    )
 }
 
 # Password validation
